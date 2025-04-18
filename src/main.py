c9593a36db7df65af9130d48c95c208e4c4deecf
yeriel <yeriel.paz@sansano.usm.cl>
2025-04-18 17:31:19 -0400

    update headers

--- src/main.py
+++ src/main.py
@@ -28,7 +28,9 @@ async def get_data(rut: str):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
         "Referer": "https://www.rutificador.co/",
-        "X-Requested-With": "XMLHttpRequest"
+        "X-Requested-With": "XMLHttpRequest",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
     }
     async with httpx.AsyncClient() as client:
         try:
